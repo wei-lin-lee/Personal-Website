1427e04d5dbef4f9cd00a1db2daed37fe8a1ee70
Wei Lin Lee <gtr8188@gmail.com>
2017-04-03 13:49:19 -0400

    nav, work, edu, Skill(TBD)

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,3 +77,19 @@ $(document).ready(function() {
     
 
 });
+
+var callback = function(){
+  $('.item-skills').each(function(){
+    newWidth = $(this).parent().width() * $(this).data('percent');
+    $(this).width(0);
+    $(this).animate({
+        width: newWidth,
+    }, 1000);
+  });
+  $('.icons-red').each(function(){
+    height = $(this).height();
+    $(this).animate({
+        height: 14,
+    }, 2000);
+  });
+};
